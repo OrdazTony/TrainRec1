e0223c499b68c817f7a1f6044e2ede761ae9f13e
Anthony Ordaz <ordaz.tony.24@gmail.com>
2026-03-29 03:03:50 -0400

    Made changes to the way router is set up. Implemented working navbar. Switched from AntDesign to MaterialUI for easier component integration.

---
+++ TrainRec/src/Components/Progress.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Progress = () => {
+    return <div className="App">Progress</div>
+    }
+export default Progress;
---
+++ TrainRec/src/Components/Workouts.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Workouts = () => {
+    return <div className="App">Workouts</div>
+    }
+export default Workouts;
